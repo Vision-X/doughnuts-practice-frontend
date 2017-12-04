4275aab23f219e74f6f7f95d32a3d7464461228a
Mike Marlow <michael.s.marlow@gmail.com>
2017-12-04 14:20:05 -0700

    fixes FETCH path

--- public/src/main.js
+++ public/src/main.js
@@ -3,7 +3,7 @@ let add = document.getElementById('append')
 
 button.addEventListener('click', (e) => {
   add.innerText = ''
-  fetch('https://still-island-35029.herokuapp.com/')
+  fetch('https://whispering-dusk-24898.herokuapp.com/')
   .then((res) => res.json())
   .then((data) => {
     data.forEach((item) => {
